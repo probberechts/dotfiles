655febb11863331d23b013fdca4c650c36e21193
Pieter Robberechts <pieter_robberechts@msn.com>
2016-12-12 13:42:32 +0100

    zplug nice is deprecated, use defer

--- zsh/zplug.zsh
+++ zsh/zplug.zsh
@@ -5,7 +5,7 @@
 # Use repo format for oh-my-zsh plugins so no random crap is sourced
 #
 # Make sure fpaths are defined before or within zplug -- it calls compinit
-# again in between loading plugins and nice plugins.
+# again in between loading plugins and defer plugins.
 #
 
 # ----------------------------------------------------------------------------
@@ -37,23 +37,20 @@ zplug "mafredri/zsh-async", on:sindresorhus/pure
 zplug "sindresorhus/pure", use:pure.zsh
 
 # ----------------------------------------------------------------------------
-# LAST, these call "compdef" so must be run after compinit, enforced by nice
+# LAST, these call "compdef" so must be run after compinit, enforced by defer
 # ----------------------------------------------------------------------------
 
-# fork of rupa/z with better completion (so needs nice)
+# fork of rupa/z with better completion (so needs defer)
 zplug "knu/z",  \
   use:"z.sh",   \
-  nice:10
-
-# ----------------------------------------------------------------------------
-# Completions that require compdef (so nice 10)
-# ----------------------------------------------------------------------------
+  defer:2
 
 # Moved if block outside since `zplug check` doesn't consider it
 if [[ $OSTYPE == "darwin"* ]]; then
   zplug "vasyharan/zsh-brew-services",  \
-    nice:10
+    defer:2
 fi
 
 # highlight as you type
-zplug "zsh-users/zsh-syntax-highlighting", nice:19
+# absolute last
+zplug "zsh-users/zsh-syntax-highlighting", defer:3
