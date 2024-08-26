061502628427105a6c2e5f971944e289f0d0249d
Pieter Robberechts <pieter.robberechts@kuleuven.be>
2024-08-26 10:30:28 +0200

    fix basedpyright cfg

--- nvim/lua/dko/tools/python.lua
+++ nvim/lua/dko/tools/python.lua
@@ -33,8 +33,6 @@ tools.register({
       settings = {
         basedpyright = {
           typeCheckingMode = "standard",
-        },
-        pyright = {
           -- Using Ruff's import organizer
           disableOrganizeImports = true,
         },
@@ -66,12 +64,31 @@ tools.register({
   mason_type = "lsp",
   require = "python",
   lspconfig = function()
+    local ruff_config_path = require("dko.utils.file").find_exists({
+      vim.loop.os_homedir() .. "/.config/ruff/ruff.toml",
+      require("dko.utils.project").root() .. "/ruff.toml",
+      require("dko.utils.project").root() .. "/pyproject.toml",
+    })
+    local args = {}
+    if ruff_config_path then
+      args = { "--config=" .. ruff_config_path }
+    end
     return {
       ---note: local on_attach happens AFTER autocmd LspAttach
       on_attach = function(client)
         -- basedpyright instead
         client.server_capabilities.hoverProvider = false
       end,
+      init_options = {
+        settings = {
+          format = {
+            args = args,
+          },
+          lint = {
+            args = args,
+          },
+        },
+      },
     }
   end,
 })
