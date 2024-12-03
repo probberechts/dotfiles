944d27803b679674b1fc158807d1dd1bd5414ceb
Pieter Robberechts <pieter.robberechts@kuleuven.be>
2024-12-03 12:11:56 +0100

    markdownpreview

--- nvim/lua/dko/plugins/markdown.lua
+++ nvim/lua/dko/plugins/markdown.lua
@@ -29,6 +29,23 @@ return {
   },
 
   {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && yarn install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
+    ft = { "markdown" },
+    config = function()
+      vim.g.mkdp_open_to_the_world = 1
+      vim.g.mkdp_open_ip = "127.0.0.1"
+      vim.g.mkdp_port = 8080
+      vim.g.mkdp_browser = "none"
+      vim.g.mkdp_echo_preview_url = 1
+    end,
+  },
+
+  {
     "lukas-reineke/headlines.nvim",
     cond = has_ui,
     dependencies = "nvim-treesitter/nvim-treesitter",
