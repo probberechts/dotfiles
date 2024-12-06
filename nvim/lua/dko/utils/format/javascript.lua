24a8e70c32e951497bc929fdc7cdd5a83f87ff16
Pieter Robberechts <pieter.robberechts@kuleuven.be>
2024-12-06 09:34:37 +0100

    fix js format

--- nvim/lua/dko/utils/format/javascript.lua
+++ nvim/lua/dko/utils/format/javascript.lua
@@ -1,22 +1,49 @@
--- NO eslint-plugin-prettier? maybe run prettier
--- then, maybe run eslint --fix
-return function(notify)
-  vim.b.has_eslint = vim.b.has_eslint
-    or #vim.lsp.get_clients({ bufnr = 0, name = "eslint" }) > 0
+local dkonode = require("dko.utils.node")
 
-  if vim.b.has_eslint then
-    if vim.b.has_eslint_plugin_prettier == nil then
-      vim.b.has_eslint_plugin_prettier =
-        require("dko.utils.node").has_eslint_plugin("prettier")
-    end
+local toast = require("dko.utils.notify").toast
 
-    vim.cmd.EslintFixAll()
-
-    if vim.b.has_eslint_plugin_prettier then
-      return notify({ "eslint-plugin-prettier" })
-    end
-    return notify({ "eslint" })
+local function format_with_lsp()
+  if vim.lsp.get_clients({ bufnr = 0, name = "eslint" }) == 0 then
+    toast("eslint-lsp not attached", vim.log.levels.WARN, {
+      group = "format",
+      title = "[LSP] eslint-lsp",
+      render = "wrapped-compact",
+    })
+    return false
   end
 
-  require("dko.utils.format.efm").format({ pipeline = "javascript" })
+  vim.cmd.EslintFixAll()
+  local formatter = vim.b.has_eslint_plugin_prettier
+      and "eslint-plugin-prettier"
+    or "eslint-lsp"
+  toast(formatter, vim.log.levels.INFO, {
+    group = "format",
+    title = "[LSP] eslint-lsp",
+    render = "wrapped-compact",
+  })
+  return true
+end
+
+return function()
+  -- Find and buffer cache prettier presence
+  if vim.b.has_eslint_plugin_prettier == nil then
+    vim.b.has_eslint_plugin_prettier = dkonode.has_eslint_plugin("prettier")
+  end
+
+  -- Run eslint via nvim-lsp eslint-lsp
+  format_with_lsp()
+
+  -- Finally, run prettier via efm if eslint-plugin-prettier not found
+  if not vim.b.has_eslint_plugin_prettier then
+    local did_efm_format =
+      require("dko.utils.format.efm").format({ pipeline = "javascript" })
+    if not did_efm_format then
+      toast("Did not format with efm/prettier", vim.log.levels.WARN, {
+        group = "format",
+        title = "[LSP] efm",
+        render = "wrapped-compact",
+      })
+    end
+    return did_efm_format
+  end
 end
