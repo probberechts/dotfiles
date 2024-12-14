4d12635337734eae83a4819a7a96986cc6648309
Pieter Robberechts <pieter.robberechts@kuleuven.be>
2024-12-14 11:56:57 +0100

    indent chunk config

--- nvim/lua/dko/plugins/ai.lua
+++ nvim/lua/dko/plugins/ai.lua
@@ -30,7 +30,7 @@ return {
       "nvim-treesitter/nvim-treesitter",
       "hrsh7th/nvim-cmp", -- Optional: For using slash commands and variables in the chat buffer
       "nvim-telescope/telescope.nvim", -- Optional: For using slash commands
-      { "stevearc/dressing.nvim", opts = {} }, -- Optional: Improves `vim.ui.select`
+      -- { "stevearc/dressing.nvim", opts = {} }, -- Optional: Improves `vim.ui.select`
     },
     opts = {
       -- display = {
