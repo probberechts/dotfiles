eab29a6769954c680e02620eb2c54556c323fbf3
Pieter Robberechts <pieter.robberechts@kuleuven.be>
2024-12-17 16:59:14 +0100

    symlink karabiner.json; iterm hotkey

--- mac/hammerspoon/launcher/apps.lua
+++ mac/hammerspoon/launcher/apps.lua
@@ -1,11 +1,11 @@
 ---
 -- Generic app keybinds
 
-print "== launcher.apps"
+print("== launcher.apps")
 
 -- a is for lunette left
 
-hs.hotkey.bind(hyper, 'b', function()
+hs.hotkey.bind(hyper, "b", function()
   hs.application.launchOrFocus("Bitwarden")
 end)
 
@@ -15,18 +15,22 @@ end)
 
 -- f for lunette
 
-hs.hotkey.bind(hyper, 'g', function()
+hs.hotkey.bind(hyper, "g", function()
   hs.application.launchOrFocus("Google Chrome")
 end)
 
 -- h for lunette
 
-hs.hotkey.bind(hyper, 'n', function()
+hs.hotkey.bind(hyper, "n", function()
   hs.application.launchOrFocus("Joplin")
 end)
 
-hs.hotkey.bind(hyper, 's', function()
+hs.hotkey.bind(hyper, "s", function()
   hs.application.launchOrFocus("Slack")
+end)
+
+hs.hotkey.bind(hyper, "t", function()
+  hs.application.launchOrFocus("iTerm")
 end)
 
 return nil
