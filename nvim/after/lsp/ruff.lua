30b024f81b0f3cca46932ac1643c22724d6eb68e
Pieter Robberechts <pieter.robberechts@kuleuven.be>
2025-05-18 14:32:30 +0200

    reset ruff config selection

--- nvim/after/lsp/ruff.lua
+++ nvim/after/lsp/ruff.lua
@@ -1,7 +1,26 @@
+local ruff_config_path = require("dko.utils.file").find_exists({
+  vim.loop.os_homedir() .. "/.config/ruff/ruff.toml",
+  require("dko.utils.project").root() .. "/ruff.toml",
+  require("dko.utils.project").root() .. "/pyproject.toml",
+})
+local args = {}
+if ruff_config_path then
+  args = { "--config=" .. ruff_config_path }
+end
 return {
   ---note: local on_attach happens AFTER autocmd LspAttach
   on_attach = function(client)
     -- basedpyright instead
     client.server_capabilities.hoverProvider = false
   end,
+  init_options = {
+    settings = {
+      format = {
+        args = args,
+      },
+      lint = {
+        args = args,
+      },
+    },
+  },
 }
