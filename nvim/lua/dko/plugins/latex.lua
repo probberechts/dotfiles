502987eae2e062331e751b46f8b63f68caf8f558
Pieter Robberechts <pieter.robberechts@kuleuven.be>
2025-05-18 11:49:31 +0200

    vimtex: do not open quickfix window automatically

--- nvim/lua/dko/plugins/latex.lua
+++ nvim/lua/dko/plugins/latex.lua
@@ -1,10 +1,7 @@
 return {
   "lervag/vimtex",
-  lazy = false, -- we don't want to lazy load VimTeX
-  -- tag = "v2.15", -- uncomment to pin to a specific release
+  lazy = false,
   init = function()
-    -- VimTeX configuration goes here
-    -- vim.g.vimtex_view_general_viewer = "okular"
-    -- vim.g.vimtex_view_general_options = "--unique file:@pdf#src:@line@tex"
+    vim.g.vimtex_quickfix_mode = 0 -- the quickfix window is not opened automatically
   end,
 }
