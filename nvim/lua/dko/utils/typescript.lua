c8cfc6324419cd05861c5baeb89463d8b5b19e5b
Pieter Robberechts <pieter.robberechts@kuleuven.be>
2025-06-23 11:48:41 +0200

    fix javascript pipeline

--- nvim/lua/dko/utils/typescript.lua
+++ nvim/lua/dko/utils/typescript.lua
@@ -46,33 +46,6 @@ M.ts_ls.config = {
 
     vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
   end,
-
-  handlers = {
-    [Methods.textDocument_publishDiagnostics] = function(_, result, ctx)
-      if not result.diagnostics then
-        return
-      end
-
-      -- ignore some ts_ls diagnostics
-      local idx = 1
-      while idx <= #result.diagnostics do
-        local entry = result.diagnostics[idx]
-
-        local formatter = require("format-ts-errors")[entry.code]
-        entry.message = formatter and formatter(entry.message) or entry.message
-
-        -- codes: https://github.com/microsoft/TypeScript/blob/main/src/compiler/diagnosticMessages.json
-        if entry.code == 80001 then
-          -- { message = "File is a CommonJS module; it may be converted to an ES module.", }
-          table.remove(result.diagnostics, idx)
-        else
-          idx = idx + 1
-        end
-      end
-
-      vim.lsp.diagnostic.on_publish_diagnostics(_, result, ctx)
-    end,
-  },
 }
 
 return M
