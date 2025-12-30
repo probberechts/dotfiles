be6294d26d87afc932a473501ae0b37706e399c6
Pieter Robberechts <pieter.robberechts@kuleuven.be>
2025-12-30 11:38:19 +0100

    ltx cmp fix

--- nvim/after/lsp/ltex.lua
+++ nvim/after/lsp/ltex.lua
@@ -1,7 +1,7 @@
 return {
   settings = {
     ltex = {
-      capabilities = require("cmp_nvim_lsp").default_capabilities(),
+      -- capabilities = require("cmp_nvim_lsp").default_capabilities(),
       checkFrequency = "edit",
       language = "en-US",
       completionEnabled = true,
