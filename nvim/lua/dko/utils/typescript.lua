0d24734268a73efefe676e5d752eb15879c354d0
Pieter Robberechts <pieter.robberechts@kuleuven.be>
2026-04-02 16:49:57 +0200

    use strings instead of enum for lsp protocol methods

--- nvim/lua/dko/utils/typescript.lua
+++ nvim/lua/dko/utils/typescript.lua
@@ -1,7 +1,5 @@
 -- Typescript specific
 
-local Methods = vim.lsp.protocol.Methods
-
 local M = {}
 
 ---Go to source definition using LSP command
@@ -28,7 +26,7 @@ M.go_to_source_definition = function(name, command)
     end
   end
 
-  return client:request(Methods.workspace_executeCommand, {
+  return client:request("workspace/executeCommand", {
     command = command,
     arguments = { position_params.textDocument.uri, position_params.position },
   }, definition_handler, 0)
@@ -60,6 +58,33 @@ M.ts_ls.config = {
       twoslash.attach(client, bufnr)
     end
   end,
+
+  handlers = {
+    ["textDocument/publishDiagnostics"] = function(_, result, ctx)
+      if not result.diagnostics then
+        return
+      end
+
+      -- ignore some ts_ls diagnostics
+      local idx = 1
+      while idx <= #result.diagnostics do
+        local entry = result.diagnostics[idx]
+
+        local formatter = require("format-ts-errors")[entry.code]
+        entry.message = formatter and formatter(entry.message) or entry.message
+
+        -- codes: https://github.com/microsoft/TypeScript/blob/main/src/compiler/diagnosticMessages.json
+        if entry.code == 80001 then
+          -- { message = "File is a CommonJS module; it may be converted to an ES module.", }
+          table.remove(result.diagnostics, idx)
+        else
+          idx = idx + 1
+        end
+      end
+
+      vim.lsp.diagnostic.on_publish_diagnostics(_, result, ctx)
+    end,
+  },
 }
 
 return M
