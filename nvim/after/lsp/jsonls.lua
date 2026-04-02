9e12fceef4b5b86a5bda7035d929d25bf5b0943d
Pieter Robberechts <pieter.robberechts@kuleuven.be>
2026-04-02 16:52:51 +0200

    add lspconfig typings

--- nvim/after/lsp/jsonls.lua
+++ nvim/after/lsp/jsonls.lua
@@ -1,6 +1,7 @@
 return {
   ---@type vim.lsp.Config
   settings = {
+    ---@type lspconfig.settings.jsonls
     json = {
       schemas = require("schemastore").json.schemas(),
       -- https://github.com/b0o/SchemaStore.nvim/issues/8#issuecomment-1129528787
